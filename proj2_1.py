0b831a5954e481bce8159144665e35c42128fb00
Nachi963 <148493723+Nachi963@users.noreply.github.com>
2023-11-24 17:47:22 -0800

    Update proj2_1.py
    with logging

--- proj2_1.py
+++ proj2_1.py
@@ -13,51 +13,70 @@
 #Note: Ensure FITS files 'red_image.fits', 'green_image.fits', and 'blue_image.fits' exist in the working directory.
 
 #"""
+import logging
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
-from skimage.transform import resize  # Importing the resize function
+from skimage.transform import resize
 
-# Load FITS files for each wavelength (red, green, blue)
-red_data = fits.getdata('red_image.fits')
-green_data = fits.getdata('green_image.fits')
-blue_data = fits.getdata('blue_image.fits')
+logging.basicConfig(filename='rgb_image_generation.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Normalize pixel values between 0 and 1 for each color channel
-red_data_normalized = (red_data - np.nanmin(red_data)) / (np.nanmax(red_data) - np.nanmin(red_data))
-green_data_normalized = (green_data - np.nanmin(green_data)) / (np.nanmax(green_data) - np.nanmin(green_data))
-blue_data_normalized = (blue_data - np.nanmin(blue_data)) / (np.nanmax(blue_data) - np.nanmin(blue_data))
+def load_fits_data(file_path):
+    logging.info(f"Loading FITS file: {file_path}")
+    data = fits.getdata(file_path)
+    return data
 
-# Replace NaN values with a default value (e.g., 0)
-red_data_normalized = np.nan_to_num(red_data_normalized, nan=0)
-green_data_normalized = np.nan_to_num(green_data_normalized, nan=0)
-blue_data_normalized = np.nan_to_num(blue_data_normalized, nan=0)
+def normalize_data(data):
+    logging.info("Normalizing pixel values between 0 and 1")
+    normalized_data = (data - np.nanmin(data)) / (np.nanmax(data) - np.nanmin(data))
+    return np.nan_to_num(normalized_data, nan=0)
 
-# Check shapes of the arrays
-print("Red shape:", red_data_normalized.shape)
-print("Green shape:", green_data_normalized.shape)
-print("Blue shape:", blue_data_normalized.shape)
+def resize_data(data, target_shape):
+    logging.info("Resizing data to match target shape")
+    return resize(data, target_shape, mode='reflect')
 
-# Resize red and blue images to match the shape of the green image
-red_data_resized = resize(red_data_normalized, green_data_normalized.shape, mode='reflect')
-blue_data_resized = resize(blue_data_normalized, green_data_normalized.shape, mode='reflect')
+def combine_channels(red, green, blue):
+    logging.info("Combining color channels into an RGB image")
+    return np.stack((red, green, blue), axis=-1)
 
-# Check the shapes after resizing
-print("Resized Red shape:", red_data_resized.shape)
-print("Resized Blue shape:", blue_data_resized.shape)
-
-# Combine the color channels into an RGB image
-if red_data_resized.shape == green_data_normalized.shape == blue_data_resized.shape:
-    rgb_image = np.stack((red_data_resized, green_data_normalized, blue_data_resized), axis=-1)
-
-    # Plot the RGB image with equatorial coordinates
+def plot_rgb_image(rgb_image):
     plt.figure(figsize=(8, 8))
-    plt.imshow(rgb_image, origin='lower')  # Set origin to 'lower' for equatorial coordinates
+    plt.imshow(rgb_image, origin='lower')
     plt.xlabel('Right Ascension')
     plt.ylabel('Declination')
     plt.title('Hubble UDF - RGB Image')
     plt.colorbar(label='Intensity')
     plt.grid(color='white', linestyle='--', linewidth=0.5)
     plt.show()
-else:
-    print("Shapes of the resized arrays are not compatible for stacking into an RGB image.")
+
+try:
+    red_data = load_fits_data('red_image.fits')
+    green_data = load_fits_data('green_image.fits')
+    blue_data = load_fits_data('blue_image.fits')
+
+    red_normalized = normalize_data(red_data)
+    green_normalized = normalize_data(green_data)
+    blue_normalized = normalize_data(blue_data)
+
+    logging.info("Shapes of the normalized arrays")
+    logging.info(f"Red shape: {red_normalized.shape}")
+    logging.info(f"Green shape: {green_normalized.shape}")
+    logging.info(f"Blue shape: {blue_normalized.shape}")
+
+    target_shape = green_normalized.shape
+    red_resized = resize_data(red_normalized, target_shape)
+    blue_resized = resize_data(blue_normalized, target_shape)
+
+    logging.info("Shapes after resizing")
+    logging.info(f"Resized Red shape: {red_resized.shape}")
+    logging.info(f"Resized Blue shape: {blue_resized.shape}")
+
+    if red_resized.shape == green_normalized.shape == blue_resized.shape:
+        rgb_image = combine_channels(red_resized, green_normalized, blue_resized)
+        plot_rgb_image(rgb_image)
+    else:
+        logging.error("Shapes of the resized arrays are not compatible for stacking into an RGB image.")
+
+except Exception as e:
+    logging.exception(f"An error occurred: {str(e)}")
+
